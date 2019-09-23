a608bdf764d5810dcd5544fef0f97f8690b83fc0
Anna Langner <annalangner06@gmail.com>
2019-09-23 11:09:01 +0200

    The first steps
    -Init a project
    -Use the create-react-app
    -Adding structure of files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MainLayout from "./components/layout/MainLayout/MainLayout";
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <MainLayout></MainLayout>
+    );
+  }
+}
+
+export default App;
