8a406299e8a543a6b2f449ecdcac63ce3ae17a7f
Anna Langner <annalangner06@gmail.com>
2019-09-23 13:28:40 +0200

    Adding declaration of state to App.js component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
-import MainLayout from "./components/layout/MainLayout/MainLayout";
+//import MainLayout from "./components/layout/MainLayout/MainLayout";
 
 class App extends React.Component {
 
+  state = {
+    product: [],
+  };
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/home')
+        .then(res => res.json())
+        .then(res => {
+          this.setState({ product: res });
+        });
+  }
+
   render() {
     return (
-      <MainLayout></MainLayout>
+      //<MainLayout></MainLayout>
+        <div>
+          <ul>
+            {this.state.product.map(product => <li key={product.id}>{product.model}</li>)}
+          </ul>
+        </div>
     );
   }
 }
