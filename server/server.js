bc3b3e5d9f7dad4549ee610647a90def03a8d498
Anna Langner <annalangner06@gmail.com>
2019-09-23 12:45:03 +0200

    Adding skeleton of server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get('/api/home', (req, res) => {
+    const data = [
+        {id: 'man(1)', brand: 'A&L', model: 'male', picture: '1'},
+        {id: 'man(2)', brand: 'A&L', model: 'male', picture: '1'},
+    ];
+    res.json(data);
+});
+
+app.listen(8000, function () {
+    console.log('Server is running on port: ', 8000);
+});
