b173295b84672610fa50e9bd58a75db92a80f28f
Anna Langner <annalangner06@gmail.com>
2019-09-27 11:54:52 +0200

    Reducer modification

--- client/src/redux/productsRedux.js
+++ client/src/redux/productsRedux.js
@@ -16,8 +16,12 @@ const reducerName = 'products';
 const createActionName = name => `app/${reducerName}/${name}`;
 
 export const LOAD_PRODUCTS = createActionName('LOAD_PRODUCTS');
+export const START_REQUEST = createActionName('START_REQUEST');
+export const END_REQUEST = createActionName('END_REQUEST');
 
 export const loadProducts = payload => ({ payload, type: LOAD_PRODUCTS});
+export const startRequest = () => ({ type: START_REQUEST });
+export const endRequest = () => ({ type: END_REQUEST });
 
 /* INITIAL STATE */
 const initialState = {
@@ -25,16 +29,21 @@ const initialState = {
         {id: 'man(1)', name: 'Product 1', brand: 'A&L', model: 'male', picture: 'man(1)', content: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'},
         {id: 'man(2)', name: 'Product 2', brand: 'A&L', model: 'male', picture: 'man(2)', content: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'},
     ],
-    cart: [{ product: 1, amount: 2 }]
+    cart: [{ product: 1, amount: 2 }],
+    request: {
+        pending: false,
+    }
 };
 
 /* REDUCER */
 export default function productsRedux(statePart = initialState, action = {}) {
     switch (action.type) {
         case LOAD_PRODUCTS:
-            return [...action.payload];
-        default:
-            return statePart;
+            return { ...statePart, data: action.payload };
+        case START_REQUEST:
+            return { ...statePart, request: { pending: true } };
+        case END_REQUEST:
+            return { ...statePart, request: { pending: false } };
     }
 };
 
