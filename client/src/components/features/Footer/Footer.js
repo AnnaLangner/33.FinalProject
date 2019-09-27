1ec459e0b3737b9acaed43182d665fb757f2c4f3
Anna Langner <annalangner06@gmail.com>
2019-09-27 20:52:44 +0200

    Develop Footer.js

--- client/src/components/features/Footer/Footer.js
+++ client/src/components/features/Footer/Footer.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import MainMenu from '../../layouts/MainMenu/MainMenu';
+import './Footer.scss'
 
 class Footer extends React.Component {
 
+    state = {
+        links: [
+            { path: '/', title: 'Home' },
+            { path: '/FAQ', title: 'FAQ' },
+            { path: '/statute', title: 'Statute' },
+            { path: '/contact', title: 'Contact' },
+        ],
+    };
+
     render() {
+        const { links } = this.state;
         return (
-            <div>
-                <p>All rights reserved</p>
-                <Link to={'/'}>Home</Link>
-                <Link to={'/FAQ'}>FAQ</Link>
-                <Link to={'/statute'}>Statute</Link>
-                <Link to='/contact'>Contact</Link>
-            </div>
+            <nav className={'footer'}>
+                <p>{'All rights reserved'}</p>
+                <MainMenu links={links}/>
+            </nav>
         );
     }
 }
