0906dc145ca4433271c9ecf58b70d8e5f7c06fa2
Anna Langner <annalangner06@gmail.com>
2019-10-03 11:38:05 +0200

    Adding content to FAQ

--- client/src/components/pages/FAQ/FaqPage.js
+++ client/src/components/pages/FAQ/FaqPage.js
@@ -3,6 +3,14 @@ import React from 'react';
 const FaqPage = () => (
     <div>
         <h1>FAQ</h1>
+        <h2>1. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+        <h2>2. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+        <h2>3. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+        <h2>4. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
     </div>
 );
 
