3fa0e59de8eeaa9184a1641761b613f1cce7368c
Anna Langner <annalangner06@gmail.com>
2019-10-03 11:18:24 +0200

    the second attempt to adding sort to SideBar

--- client/src/components/features/SideBar/SideBar.js
+++ client/src/components/features/SideBar/SideBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Container, Badge, Button } from 'reactstrap';
 import loadTestData from '../../../testData';
 
 class SideBar extends React.Component {
@@ -29,9 +29,23 @@ class SideBar extends React.Component {
 
     render() {
         return (
-            <Row>
-                <Col data={this.state.data} sortBy={this.sortBy}/>
-            </Row>
+            <Container>
+                <div>
+                    <h1>Heading <Badge color="secondary">Sort by:</Badge></h1>
+                </div>
+                <Row>
+                    <Col><Button color="link">Name A-Z</Button></Col>
+                </Row>
+                <Row>
+                    <Col><Button color="link">Name Z-A</Button></Col>
+                </Row>
+                <Row>
+                    <Col data={this.state.data} sortBy={this.sortBy}><Button color="link">Price up</Button></Col>
+                </Row>
+                <Row>
+                    <Col><Button color="link">Price down</Button></Col>
+                </Row>
+            </Container>
         )
     }
 }
