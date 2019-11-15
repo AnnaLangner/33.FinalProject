d2be011e5caec396809938661d13f43647f0abf9
Anna Langner <annalangner06@gmail.com>
2019-11-15 18:03:36 +0100

    Changing page layout

--- client/src/components/pages/Cart/CartPage.js
+++ client/src/components/pages/Cart/CartPage.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Row, Col } from 'reactstrap';
+import { Container } from 'reactstrap';
 import Cart from '../../features/Cart/CartContainer'
 
 
 const CartPage = () => (
     <Container>
-        <Row>
-            <Col xs={9}>
-                <Cart/>
-            </Col>
-            <Col xs={3}>
-                <p>okienka zwiększające ilość rzeczy w koszyku </p>
-            </Col>
-        </Row>
+        <Cart/>
     </Container>
 );
 
