dcce5fb551215ea9dc9af2b40cd12f8fbea519cf
Anna Langner <annalangner06@gmail.com>
2019-11-27 20:55:45 +0100

    Adding Alert to Cart.js

--- client/src/components/features/Cart/Cart.js
+++ client/src/components/features/Cart/Cart.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import Button from '../../common/Button/Button';
+import PageTitle from '../../common/PageTitle/PageTitle';
+import Alert from '../../common/Alert/Alert'
 import './Cart.css';
 
 class Cart extends React.Component {
@@ -16,9 +18,9 @@ class Cart extends React.Component {
         const {products, increaseProductAmount, decreaseProductAmount } = this.props;
         return (
             <div>
+                <PageTitle>Shopping Cart</PageTitle>
                 <ul>
-                    {products.length}
-                    {products.map(item =>
+                    {products.length !== 0 ? products.map(item =>
                         <li>
                             {item.product.name}
                             <Button onClick={ () => {decreaseProductAmount(item.product._id)}} variant={'danger'} className={'buttonClick'}>-</Button>
@@ -26,7 +28,7 @@ class Cart extends React.Component {
                             <Button onClick={ () => {increaseProductAmount(item.product._id)}} variant={'success'} className={'buttonClick'}>+</Button>
                             <Button onClick={ () => {this.handleRemoveProduct(item.product._id)} } variant={'danger'}>Delete</Button>
                         </li>
-                    )}
+                    ) : <Alert variant={'danger'}>Your shopping cart is empty!</Alert>}
                 </ul>
             </div>
         );
@@ -42,7 +44,8 @@ Cart.propTypes = {
             price: PropTypes.string.isRequired,
             amount: PropTypes.number.isRequired
         }).isRequired
-    ).isRequired
+    ).isRequired,
+    removeCartProduct: PropTypes.func.isRequired,
 };
 
 export default withRouter(props => <Cart {...props}/>);
